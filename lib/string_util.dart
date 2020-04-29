155b87914e8ebcc1c45db799ebdc257c3078073c
Dmitrii Zolotov <d.zolotov@baibol.ru>
2020-04-29 21:55:13 +0300

    fix mixin

--- lib/string_util.dart
+++ lib/string_util.dart
@@ -1,3 +1,7 @@
-extension UserUtils on String {
+mixin UserUtils {
+  String capitalize(String s) => s[0].toUpperCase() + s.substring(1).toLowerCase();
+}
+
+extension StringUtils on String {
   String capitalize(String s) => s[0].toUpperCase() + s.substring(1).toLowerCase();
 }
